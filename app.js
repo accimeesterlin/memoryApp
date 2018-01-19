09dc273039514f6b358f8b13857d2fa19abeef88
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2018-01-19 13:42:41 -0800

    Changed https to http back

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import db from './server/models';
 
 const app = App(__dirname);
 
-const server = require('https').Server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 //use sync({force:true}) to drop all tables before trying to create
