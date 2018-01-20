6bd039855ac8459b47bfa19332fa28cf243d108c
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2018-01-20 13:28:00 -0800

    Auto stash before merge of "notifications" and "master"

--- client/src/Components/Notifications/socket.js
+++ client/src/Components/Notifications/socket.js
@@ -1,6 +1,7 @@
 import openSocket from 'socket.io-client';
 
-const  socket = openSocket('https://planttheseed.herokuapp.com');
+const herokuURL = 'https://planttheseed.herokuapp.com';
+const socket = location.protocol === 'https:' ? openSocket(herokuURL) : openSocket();
 
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
